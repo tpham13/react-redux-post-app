2f1e2e00ec9d4fb4248e7f3a1b0f69cdaf3416c2
tpham13 <tpham0123@gmail.com>
2021-02-04 23:41:20 -0800

    Create post form and made a post fetch

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Posts from './components/Posts';
+import Postform from './components/Postform';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Postform />
+        <hr />
         <Posts />
       </div>
     );
--- src/components/Postform.js
+++ src/components/Postform.js
@@ -1,16 +1,64 @@
 import React from 'react';
 
 class Postform extends React.Component {
-constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-    };
-}
+        this.state = {
+            title: '',
+            body: ''
+        };
+        //we have to bind onChange to this here b/c it doesn't know what this is
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const post = {
+            title: this.state.title,
+            body: this.state.body
+        }
+
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(post)
+        })
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }
 
     render() {
-        return <div></div>;
+        return (
+            <div>
+                <h1>Postform </h1>
+                <form onSubmit={this.onSubmit}>
+                    <div>
+                        <label> Title: </label>
+                        <input type="text" name="title" onChange={this.onChange} value={this.state.title} />
+                    </div>
+                    <br />
+                    <div>
+                        <label> Body: </label>
+                        <input type="text" name="body" onChange={this.onChange} value={this.state.body} />
+                    </div>
+                    <br />
+                        <button type="submit">Submit</button>
+                    
+                </form>
+                
+            </div>
+        )
     }
+    
 }
 
 
