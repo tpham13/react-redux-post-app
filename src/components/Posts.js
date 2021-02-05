8c003cfe6cff8153f2cfff7a9d06cf939da3d617
tpham13 <tpham0123@gmail.com>
2021-02-04 23:04:44 -0800

    Add note in Posts.js

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,22 +1,39 @@
 import React from 'react';
 
+/*
+React(before implement Redux):
+First set the state (posts to an empty []), 
+then make a fetch request to the post fake api, 
+then setState() to fill posts: [] with the data that we got from fetch, 
+then we need to render the posts by map over the posts array, 
+then render title and body of each post element 
+*/ 
 class Posts extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            posts: []
         };
     }
 
     componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/posts')
             .then(res => res.json())
-            .then(data => console.log(data));
+            // when we make a fetch request we want to fill the posts empty array(in state) with this data
+            .then(data => this.setState({ posts: data }));
     }
     render() {
+        const postItems = this.state.posts.map(post => (
+            <div key={post.id}>
+                <h3>{post.title}</h3>
+                <p>{post.body}</p>
+            </div>
+        ))
         return (
             <div>
                 <h1>Posts </h1>
+                {postItems}
             </div>
         )
     }
