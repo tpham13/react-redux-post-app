75a69cfcbca1744a74bc7b05797a3b8178785eb8
tpham13 <tpham0123@gmail.com>
2021-02-04 22:59:30 -0800

    Made fetch request for posts successfully in fake api

---
+++ src/components/Posts.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Posts extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+        };
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(res => res.json())
+            .then(data => console.log(data));
+    }
+    render() {
+        return (
+            <div>
+                <h1>Posts </h1>
+            </div>
+        )
+    }
+}
+
+
+export default Posts;
